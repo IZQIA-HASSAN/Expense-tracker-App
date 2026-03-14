077ccdb478af37a7212d6a15b1545044c0c5e38c
Izqiaulhassan <izqiaulhassan166@gmail.com>
2026-03-14 12:29:37 +0500

    flies added

---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className='border-4 border-blue-600 h-45 w-70 p-5 rounded-md  shadow-2xl'>
+<p className='font-light'>Current balance</p>
+<h1 className='text-3xl text-blue-600 font-bold p-2'> $0.00</h1>
+</div>
+    </div>
+  )
+}
+
+export default Card
