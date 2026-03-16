20e99ce0c094fd0606b709eff4d31e2407c25951
Izqiaulhassan <izqiaulhassan166@gmail.com>
2026-03-16 23:39:06 +0500

    code added for graphs

--- src/Card3.jsx
+++ src/Card3.jsx
@@ -7,55 +7,63 @@ const Card3 = () => {
         <div className='border-4 border-purple-600 h-45 w-70 p-5 rounded-md shadow-2xl '>
 <p className='font-light'>Budget manager</p>
 <h1 className='text-2xl text-purple-600 font-bold p-2'> 2 category limits</h1>
-<button className='bg-purple-100 h-10 w-full rounded-md  text-purple-600 font-bold mt-4 transition-all ease-out duration-150 hover:scale-105 hover:bg-purple-100'>Set budget</button>
+<button onClick={()=>setBudget(true)} className='bg-purple-100 h-10 w-full rounded-md  text-purple-600 font-bold mt-4 transition-all ease-out duration-150 hover:scale-105 hover:bg-purple-100'>Set budget</button>
 {budget &&(
-    <div className="fixed inset-0 flex items-center justify-center backdrop-blur-sm bg-black/30 z-40">
-    
-    <div className="h-[410px] w-[450px] bg-white rounded-2xl shadow-xl">
-    
-      <div className="border-b border-gray-300 h-15 flex justify-between items-center">
-        <h1 className="p-3 text-2xl font-bold">Add income</h1>
-    
-        <button
-          className="rounded-full hover:bg-gray-300 mr-4 p-2 transition"
-          onClick={()=>setBudget(false)}
-        >
-          <img className="h-5" src={cross} alt="" />
+  <div className="fixed inset-0 flex items-center justify-center backdrop-blur-sm bg-black/30 z-40">
+  
+  <div className="h-[350px] w-[450px] bg-white rounded-2xl shadow-xl">
+  
+    <div className="border-b border-gray-600 h-15 flex justify-between items-center p-3">
+      <h1 className="p-3 text-xl font-bold">Set Monthly budget</h1>
+  
+      <button
+        className="rounded-full hover:bg-gray-300 mr-4 p-2 transition"
+        onClick={()=>setBudget(false)}
+      >
+        <img className="h-5" src={cross} alt="" />
+      </button>
+    </div>
+  
+    <div className="flex flex-col  p-4">
+  
+      <div className="flex flex-col p-3">
+        <label className="text-sm font-medium mb-1 text-gray-500">Category Name</label>
+        {/* <input className="border border-gray-300 rounded-md h-10 p-2 focus:outline-none "
+        type="text" placeholder="eg. Salary, Freelance" /> */}
+          <select className='border border-gray-300 h-10 rounded-md focus:outline-none'  name="" id="">
+          
+            <option value="">select category</option>
+          <option value="food">food</option>
+          <option value="transport">Transport</option>
+          <option value="utilities">utilities</option>
+          <option value="entertainment">Entertainment</option>
+          <option value="health">health</option>
+          <option value="shopping">shopping</option>
+          <option value="education">education</option>
+          <option value="other">other</option>
+          
+        </select>
+      </div>
+  
+      <div className="flex flex-col p-3">
+        <label className="text-sm font-medium mb-1 text-gray-500"> Monthly budget</label>
+        <input className="border border-gray-300 rounded-md h-10 p-2 focus:outline-none "
+        type="number" placeholder="0.0" />
+      </div>
+  
+      
+  
+      <div className="flex justify-center mt-5 px-3">
+        <button className="w-full transition-all ease-out duration-100 hover:scale-101 rounded-md h-10 bg-blue-600 hover:bg-blue-700 text-white">
+          Save budget
         </button>
       </div>
-    
-      <div className="flex flex-col">
-    
-        <div className="flex flex-col p-3">
-          <label className="text-sm font-medium mb-1">Name</label>
-          <input className="border border-gray-300 rounded-md h-10 p-2 focus:outline-none "
-          type="text" placeholder="eg. Salary, Freelance" />
-        </div>
-    
-        <div className="flex flex-col p-3">
-          <label className="text-sm font-medium mb-1">Amount ($)</label>
-          <input className="border border-gray-300 rounded-md h-10 p-2 focus:outline-none "
-          type="number" placeholder="0.0" />
-        </div>
-    
-        <div className="flex flex-col p-3">
-          <label className="text-sm font-medium mb-1">Date</label>
-          <input className="border border-gray-300 rounded-md h-10 p-2 focus:outline-none  "
-          type="date" />
-        </div>
-    
-        <div className="flex justify-center mt-5 px-3">
-          <button className="w-full transition-all ease-out duration-100 hover:scale-101 rounded-md h-10 bg-blue-600 hover:bg-blue-700 text-white">
-            Save income
-          </button>
-        </div>
-    
-      </div>
-    
+  
     </div>
-    
-    </div>
-    
+  
+  </div>
+  
+  </div>
 )}
 </div>
     </div>
--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -3,9 +3,114 @@ import Card from './Card'
 import Card1  from './Card1'
 import Card2 from './Card2'
 import Card3 from './Card3'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  ArcElement,
+  PointElement,
+  Tooltip,
+  Legend
+}from "chart.js"
 
+import {Bar , Line , Doughnut} from "react-chartjs-2"
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  ArcElement,
+  PointElement,
+  Tooltip,
+  Legend
+)
+
+// bar data
+const barData = {
+  labels:["Food" , "Transport"],
+  datasets:[
+    {
+      label:"Actual spent",
+      data : [100 ,0 ],
+      backgroundColor: "#ff4d6d",
+      barThickness:15,
+    },
+    {
+      label:"Budget Limit",
+      data : [10 , 200],
+      backgroundColor : "#4361ee",
+      barThickness:15,
+    },
+  ],
+}
+
+const linedata = {
+  labels : ["2026-03-13"],
+  datasets:[
+    {
+      label:"Spending",
+      data : [100],
+      borderColor :"#4361ee",
+      backgroundColor:"#4361ee",
+    },
+  ],
+}
+
+const Doughnutdata = {
+  labels :["Food"],
+  datasets:[
+    {
+      data:[100],
+      backgroundColor:["#4361ee"],
+      cutout:"80%"
+    }
+  ]
+}
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "bottom",
+      labels: {
+        boxWidth: 12,
+        padding: 20,
+        color: "#6b7280",
+        font: {
+          size: 12,
+        },
+      },
+    },
+  },
+
+  scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+      ticks: {
+        color: "#6b7280",
+        font: {
+          size: 12,
+        },
+      },
+    },
+
+    y: {
+      grid: {
+        color: "#f1f5f9",
+      },
+      ticks: {
+        color: "#6b7280",
+      },
+    },
+  },
+};
 const Dashboard = () => {
   return (
+    <>
     <div className=' flex gap-10 items-center justify-center mt-10'>
         {/* no one div */}
 <Card/>
@@ -15,7 +120,26 @@ const Dashboard = () => {
 <Card1/>
 {/* no 4 div */}
 <Card3/>
+
     </div>
+    <div className=' flex gap-10 justify-center border items-center  '>
+      <div className="bg-white p-5 rounded-xl shadow-md">
+
+  <h2 className="text-lg font-semibold mb-4 text-gray-800">
+    Budget vs. Actual Spending
+  </h2>
+
+  <Bar data={barData} options={options} />
+
+</div>
+<div className='border h-70 w-70 p-2 flex justify-center items-center'>
+<div className='h-45 w-45  '><Doughnut   data={Doughnutdata}/></div>
+</div>
+</div>
+<div className='h-60 border w-[500px] m-2'><Line data={linedata}/></div>
+
+
+    </>
   )
 }
 
